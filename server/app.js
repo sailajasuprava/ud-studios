a19ca44d8bfd8a016f420b0b865f6e94fc10d45a
sailajasuprava <sailaja.suprava@gmail.com>
2025-05-10 19:15:08 +0530

    project initialized

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const AppError = require("./utils/appArror");
+const globalErrorHandler = require("./controllers/errorController");
+
+const corsOptions = {
+  origin: " http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+
+const app = express();
+
+//MIDDLEWARES
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "4Mb" }));
+
+//ROUTES
+
+//CUSTOM ERROR MESSAGE FOR UNHANDLED ROUTES
+app.all("*", (req, res, next) => {
+  next(new AppError(`This route ${req.originalUrl} doesn't exist.`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
