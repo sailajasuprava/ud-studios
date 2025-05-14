e7b4308bcf5d989ea9da866ae0b614d4d3f33087
sailajasuprava <sailaja.suprava@gmail.com>
2025-05-14 10:04:28 +0530

    google oauth

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,14 @@
 function Login() {
+  const handleLogin = () => {
+    window.open("http://localhost:8000/api/auth/google", "_self");
+  };
+
   return (
     <div style={{ padding: "20px" }}>
       <h1> Welcome to Unsplash Explorer</h1>
       <p>Please login with one of the providers below:</p>
-      <a href="http://localhost:8000/api/auth/google">Login with Google</a>
+
+      <button onClick={handleLogin}>Login with Google</button>
       <br />
       <a href="http://localhost:8000/api/auth/github">Login with GitHub</a>
       <br />
--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const passport = require("passport");
+const session = require("express-session");
+require("./config/passport");
 
 const AppError = require("./utils/appArror");
 const globalErrorHandler = require("./controllers/errorController");
 const searchRouter = require("./routes/searchRoutes");
+const authRouter = require("./routes/authRoutes");
 
 const corsOptions = {
   origin: " http://localhost:5173",
@@ -17,8 +21,20 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "4Mb" }));
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 //ROUTES
-app.use("/api/v1/searches", searchRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/searches", searchRouter);
 
 //CUSTOM ERROR MESSAGE FOR UNHANDLED ROUTES
 app.all("*", (req, res, next) => {
